15c5305b58b04cf335652773aa6727694328a09c
Dan Leonard <dcl525@gmail.com>
2022-11-19 22:51:34 +0000

    Merged PR 997: Update Routing
    Update routing

--- services/gateway/scripts/buildup.sh
+++ services/gateway/scripts/buildup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 config="configs/kube-tools/gateway/config.json"
-routing="configs/kube-tools/gateway/routing.json"
+# routing="configs/kube-tools/gateway/routing.json"
 
 az storage blob download --account-name stazureks --container-name build-resources --name $config --file config.json
-az storage blob download --account-name stazureks --container-name build-resources --name $routing --file routing.json
+# az storage blob download --account-name stazureks --container-name build-resources --name $routing --file routing.json
 
 cp config.json services/gateway
-cp routing.json services/gateway
+# cp routing.json services/gateway
