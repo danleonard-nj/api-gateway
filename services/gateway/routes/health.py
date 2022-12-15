3adc517fa5ed9d159aafdf33f458204128354fb9
Dan Leonard <dcl525@gmail.com>
2022-12-15 10:31:47 +0000

    Merged PR 1048: Fix Health Routes
    Fix health routes

--- services/gateway/routes/health.py
+++ services/gateway/routes/health.py
@@ -1,13 +1,13 @@
-from flask import Blueprint
+from quart import Blueprint
 
 health_bp = Blueprint('health_bp', __name__)
 
 
 @health_bp.route('/api/health/alive')
-def alive(container):
+async def alive():
     return {'status': 'ok'}, 200
 
 
 @health_bp.route('/api/health/ready')
-def ready(container):
+async def ready():
     return {'status': 'ok'}, 200
